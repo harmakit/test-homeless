03cb011e8836650999e362d926b252ea3903c36d
harmakit <harmakit@gmail.com>
2020-01-25 16:47:55 +0700

    Updates

---
+++ precache-manifest.7fb4d0dcb6dde4763985fae14f27b33a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7774286ef4e87101bd47f09044afebe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b6452617e50f56d258e",
+    "url": "/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "88c3f43019815c62a9c7",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "3b6452617e50f56d258e",
+    "url": "/static/js/2.d35b7d16.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d35b7d16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88c3f43019815c62a9c7",
+    "url": "/static/js/main.ea966d15.chunk.js"
+  },
+  {
+    "revision": "f14ab066ca5e14b1ef8f",
+    "url": "/static/js/runtime-main.3eb73ea6.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
